6baf52b44c5c9067c694e295c6aef7abf8e4600e
Matt Wilkinson <mytematt@gmail.com>
2018-08-29 18:57:41 -0600

    added list and items models

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,10 @@
 class Store < ApplicationRecord
+  #validations
+
+  #associations
+    has_many :lists, dependent: :destroy
+  #callbacks
+
+  #Instance .class methods
+
 end
---
+++ db/migrate/20180830005229_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.integer :code
+      t.string :price
+      t.belongs_to :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
